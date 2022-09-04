7b9fdf2e66c0e6fc81a37444b3b94f05f03dd977
jhhan00 <jhhan009@gmail.com>
2022-09-04 23:56:37 +0900

    초기 설정 완료

---
+++ src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Container!!
+    <Post></Post>
+    <Post></Post>
+  </div>
+</template>
+
+<script>
+
+import Post from './Post.vue';
+
+export default {
+  name : "ContainerComponent",
+  components : {
+    Post
+  },
+}
+</script>
+
+<style>
+
+</style>
