20e2eecfc3809b48dafea70a4f12787ff62e648a
jhhan00 <jhhan009@gmail.com>
2022-10-28 23:30:57 +0900

    mapState 적용

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,18 @@
     <img src="./assets/logo.png" class="logo" />
   </div>
 
-  <h4>Hello {{ $store.state.name }}</h4>
+  <!-- <h4>Hello {{ $store.state.name }}</h4>
   <h5>age : {{ $store.state.age }}</h5>
   <button @click="$store.commit('nameChange')">vuex 변경1</button>
   <button @click="$store.commit('agePlus', 3)">vuex 변경2</button>
 
   <p>{{ $store.state.morePost }}</p>
-  <button @click="$store.dispatch('getMorePostData')">더보기</button>
+  <button @click="$store.dispatch('getMorePostData')">더보기</button> -->
+
+  <h5>computed : {{ getName }}</h5>
+  <h5>mapState [] : {{name}} {{age}} {{likes}}</h5>
+  <h5>mapState {} : {{stateName}} {{stateAge}} {{stateLikes}}</h5>
+  <button @click="agePlus(5)">click..</button>
 
   <Container
     :postData="instaData"
@@ -43,6 +48,7 @@
 import Container from './components/Container.vue';
 import postData from './js/postData';
 import axios from 'axios';
+import {mapMutations, mapState} from 'vuex'
 
 export default {
   name: 'App',
@@ -65,7 +71,20 @@ export default {
       this.newFilter = filter;
     });
   },
+  computed : {
+    // computed : 계산 결과 저장용 함수 모음들
+    // 처음에만 실행되고, 나중에는 실행이 되지 않음
+    // 실제 사용 시 소괄호는 빼고 사용하기
+    // 성능이 좋아질 수 있음
+    // 주의 - 항상 return 해줘야 함
+    getName() {
+      return this.$store.state.name;
+    },
+    ...mapState(['name', 'age', 'likes']),
+    ...mapState({ stateName : 'name', stateAge : 'age', stateLikes : 'likes' }),
+  },
   methods : {
+    ...mapMutations(['agePlus']),
     btn_more() {
       let url = `https://codingapple1.github.io/vue/more${this.clickCnt}.json`;      
       this.clickCnt++;
